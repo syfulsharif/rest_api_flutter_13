5b6d2b0bf4f06cc7de255c997d9797fbaf963ad8
syfulsharif <sharif.syful@gmail.com>
2024-09-24 23:25:54 +0600

    Code Updated with increament and decrement button

--- lib/home_page_view.dart
+++ lib/home_page_view.dart
@@ -8,12 +8,39 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+  int countNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Module 13 REST API Recorded'),
         centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Count : ${countNumber.toString()}'),
+      ),
+      floatingActionButton: Row(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              countNumber += 1;
+              setState(() {});
+            },
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            width: 18.0,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              countNumber -= 1;
+              setState(() {});
+            },
+            child: const Icon(Icons.remove),
+          )
+        ],
       ),
     );
   }
